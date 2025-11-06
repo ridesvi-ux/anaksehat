f97ec936460d2a90e7296077a02f8874fb717979
ridesvi-ux <ridesvi@gmail.com>
2025-11-07 03:01:54 +0700

    maindart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:anak_sehat_proyek/screens/formandresult_screen.dart';
 import 'package:anak_sehat_proyek/screens/home_screen.dart';
 import 'package:anak_sehat_proyek/screens/onboarding_screen.dart';
+import 'package:anak_sehat_proyek/screens/welcome_screen.dart';
 import 'package:flutter/material.dart';
+
 // Import semua screen yang lu punya
 void main() {
   runApp(const MyApp());
@@ -15,21 +17,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'SehatAnak',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: 'Inter',
-      ),
-      // SOLUSI 1: Ganti home ke Onboarding kalau mau mulai dari onboarding
-      // atau ke HomeScreen kalau mau langsung ke home
-      home: const Onboarding_screen(), // Atau HomeScreen()
-      
-      // SOLUSI 2: Pakai named routes (lebih proper)
-      initialRoute: '/onboarding', // atau '/home'
-      routes: {
-        '/onboarding': (context) => const Onboarding_screen(),
-        '/home': (context) => const HomeScreen(),
-        '/form': (context) => const FormPage(),
-      },
+      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Inter'),
+      home: const WelcomeScreen(), // ✅ Halaman pertama
     );
   }
 }
